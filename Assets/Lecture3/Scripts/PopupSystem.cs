8aa5de666ed4b8c3a41aeecfcf761b2a1ca81c1d
Eva <yw4147@nyu.edu>
2021-03-15 21:10:56 +0800

    Midterm prototype

---
+++ Assets/Lecture3/Scripts/PopupSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PopupSystem : MonoBehaviour
+{
+    public GameObject popUpBox;
+    public Animator animator;
+    public TMP_Text popUpText;
+
+    public void Popup(string text){
+        popUpBox.SetActive(true);
+        popUpText.text = text;
+        animator.SetTrigger("pop");
+    }
+}
